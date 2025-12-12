b4c7da64139e96d6d1866a96e462a8350dd2fe87
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2025-12-12 01:27:13 -0600

    Change in membership models, now price is associated with the type

--- apps/memberships/models.py
+++ apps/memberships/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from django.utils import timezone
 
@@ -8,6 +9,13 @@ class Membership(models.Model):
         PLUS = "plus", "Plus"
         VIP = "vip", "VIP"
 
+    # TODO Ajustes reales de open gym
+    TYPE_PRICES = {
+        Type.BASIC: Decimal("20000.00"),
+        Type.PLUS:  Decimal("30000.00"),
+        Type.VIP:   Decimal("45000.00"),
+    }
+
     client = models.ForeignKey(
         "clients.Client",
         on_delete=models.CASCADE,
@@ -15,7 +23,10 @@ class Membership(models.Model):
     )
 
     type = models.CharField(max_length=20, choices=Type.choices)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    # Se calcula automáticamente según el tipo
+    price = models.DecimalField(max_digits=8, decimal_places=2, editable=False)
+
     description = models.TextField(blank=True)
 
     start_date = models.DateField(default=timezone.now)
@@ -26,9 +37,13 @@ class Membership(models.Model):
 
     is_active = models.BooleanField(default=True)
 
+    def save(self, *args, **kwargs):
+        self.price = self.TYPE_PRICES.get(self.type, Decimal("0.00"))
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.client} - {self.get_type_display()}"
 
-    # Regla pro: solo una activa por cliente (implementarlo en lógica)
+    # TODO: solo una activa por cliente (implementarlo en lógica)
     # - En save() o en el servicio/endpoint:
     #   si self.is_active True => desactivar otras memberships del mismo client
